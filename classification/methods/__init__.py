dc073bbba0c1858496192685a2e2b676f8f814e4
weesunghyun <wsh05@snu.ac.kr>
2025-07-30 13:26:20 +0900

    Add TTN method for classification

--- classification/methods/__init__.py
+++ classification/methods/__init__.py
@@ -18,10 +18,11 @@ from methods.cmf import CMF
 from methods.deyo import DeYO
 from methods.vte import VTE
 from methods.tpt import TPT
+from methods.ttn import TTN
 
 __all__ = [
     'Source', 'BNTest', 'BNAlpha', 'BNEMA', 'TTAug',
     'CoTTA', 'RMT', 'SANTA', 'RoTTA', 'AdaContrast', 'GTTA',
     'LAME', 'MEMO', 'Tent', 'EATA', 'SAR', 'RPL', 'ROID',
-    'CMF', 'DeYO', 'VTE', 'TPT',
+    'CMF', 'DeYO', 'VTE', 'TPT', 'TTN',
 ]
